6d4ac814eaefd3f3335c63d815b4c8c54428f919
pineapple-bois <91347851+pineapple-bois@users.noreply.github.com>
2023-07-13 19:54:40 +0100

    Removed z exponent

--- poly_dictionary.py
+++ poly_dictionary.py
@@ -5,31 +5,28 @@ def decompose_polynomial(formula: str):
     """
     The input formula should be a string representation of the polynomial.
 
-    Polynomial exponents are natural numbers.
+    Conic exponents are natural numbers.
     If exponents are rationals or negative integers, the formula will not parse
 
     :return Polynomial dictionary;
-    {(exponent_x, exponent_y, exponent_z): coefficient}
+    {(exponent_x, exponent_y): coefficient}
     """
-
     if not isinstance(formula, str):
-        raise TypeError("Input must be a string representation of a polynomial.")
+        raise TypeError("Input must be a string representation of a conic.")
 
     formula = formula.replace(' ', '').replace('^', '**')
-    # Extract all variables in formula
     variables = set(re.findall('[a-z]', formula))
 
-    # Check that the variables are only x, y, and z
-    if variables.difference({'x', 'y', 'z'}):
-        raise ValueError("For multivariate polynomials, variables can only be 'x', 'y', or 'z'")
+    if variables.difference({'x', 'y'}):
+        raise ValueError("Variables can only be 'x' or 'y'")
 
     terms = re.findall(
-        r'[+-]?(?:\d+(?:\.\d*)?|\.\d+)(?:\*[a-z](?:\*\*\d+)?)*(?:\*[a-z](?:\*\*\d+)?)*|[+-]?'
-        r'(?:[a-z](?:\*\*\d+)?)*(?:\*[a-z](?:\*\*\d+)?)*|[+-]?[a-z]', formula)
+        r'[+-]?(?:\d+(?:\.\d*)?|\.\d+)(?:\*[a-z](?:\*\*\d+)?)*|[+-]?'
+        r'(?:[a-z](?:\*\*\d+)?)*|[+-]?[a-z]', formula)
 
     terms = [term for term in terms if term]  # remove any empty strings
-    terms = [term.replace('**', '^') for term in terms]  # analysis is simpler with only one iteration of '*'
-    # Constructing the dictionary
+    terms = [term.replace('**', '^') for term in terms]
+
     poly_dict = {}
     for term in terms:
         if term.startswith('+'):
@@ -38,7 +35,6 @@ def decompose_polynomial(formula: str):
         if '*' in term:
             parts = term.split('*')
 
-            # Extract and handle the coefficient if it exists
             if parts[0].replace('.', '').isdigit() or parts[0].replace('-', '').replace('.', '').isdigit():
                 coefficient = float(parts.pop(0))
             else:
@@ -46,11 +42,9 @@ def decompose_polynomial(formula: str):
                 if parts[0].startswith('-'):
                     parts[0] = parts[0][1:]
 
-            # Initialize list of exponents
-            exponents = [0, 0, 0]
+            exponents = [0, 0]
 
             for part in parts:
-                # Each part is either of the form 'x', 'x^2', etc.
                 if '^' in part:
                     variable, exponent = part.split('^')
                     exponent = int(exponent)
@@ -62,14 +56,11 @@ def decompose_polynomial(formula: str):
                     exponents[0] = exponent
                 elif variable == 'y':
                     exponents[1] = exponent
-                elif variable == 'z':
-                    exponents[2] = exponent
 
-            # The key in the dictionary is a tuple of exponents
             tuple_key = tuple(exponents)
             poly_dict[tuple_key] = coefficient
 
-        elif '^' in term:  # Handle terms like '+x^2', '+y^2', etc.
+        elif '^' in term:
             parts = term.split('^')
             variable = parts[0]
             if variable.startswith('-'):
@@ -79,39 +70,33 @@ def decompose_polynomial(formula: str):
                 coefficient = 1.0
             exponent = int(parts[1])
 
-            exponents = [0, 0, 0]
+            exponents = [0, 0]
 
             if variable == 'x':
                 exponents[0] = exponent
             elif variable == 'y':
                 exponents[1] = exponent
-            elif variable == 'z':
-                exponents[2] = exponent
 
             tuple_key = tuple(exponents)
             poly_dict[tuple_key] = coefficient
 
         elif term.isalpha() or (len(term) > 1 and term[1:].isalpha()):
-            # statement for terms like '+x', '-y' etc ... no explicit coefficient
             variable = term[-1]
             coefficient = 1.0 if term[0] != '-' else -1.0
-            exponents = [0, 0, 0]
+            exponents = [0, 0]
 
             if variable == 'x':
                 exponents[0] = 1
             elif variable == 'y':
                 exponents[1] = 1
-            elif variable == 'z':
-                exponents[2] = 1
 
             tuple_key = tuple(exponents)
             if tuple_key not in poly_dict:
                 poly_dict[tuple_key] = coefficient
 
         elif term.lstrip('+-').replace('.', '').isdigit():
-            # statement for constant terms like '+2', '-1.5', '3' etc.
             coefficient = float(term)
-            poly_dict[(0, 0, 0)] = coefficient
+            poly_dict[(0, 0)] = coefficient
 
     return poly_dict
 
